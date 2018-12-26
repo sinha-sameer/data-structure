e9b049c9cf46875a077382ffdc8e19b73b5e972a
sinha-sameer <ss9711.stern.nyu.edu>
2018-12-26 02:45:55 -0500

    test

---
+++ src/SimpeQuestions/SwapNumbers.java
@@ -0,0 +1,17 @@
+package SimpeQuestions;
+
+public class SwapNumbers {
+
+	static int a = 10;
+	static int b = 5;
+	
+	public static void swapNumbers() {
+		System.out.println(a);
+	}
+	
+	public static void main(String[] args) {
+		swapNumbers();
+	}
+	
+	
+}
